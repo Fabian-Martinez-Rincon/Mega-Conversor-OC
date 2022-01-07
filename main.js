66ce2d16aeb694a49bcf13542d380b22b9fb7fa4
Fabo xw <fabianmartinezrincon.123@gmail.com>
2022-01-07 02:59:59 -0300

    acomodando codigo

--- main.js
+++ main.js
@@ -3,12 +3,65 @@ const input_decimal = document.querySelector("#input_decimal");
 
 const output2 = document.querySelector("#output2");
 const output3 = document.querySelector("#output3");
+const output4 = document.querySelector("#output4");
+const output5 = document.querySelector("#output5");
 const btn = document.querySelector("#btn");
 const error = document.querySelector("#error-msg");
 
 //________________________________________________________________________________________
+function BSS(binArr,Bss){
+    let decNo = 0;
 
-function Bin2Dec(numero) {
+    binArr.forEach((item, index) => item === '1' ? decNo += Math.pow(2, index) : void 0);
+
+    output2.value = "BSS: "+ Bss + "   Decimal: " + decNo.toString()  ;
+    output2.style.cursor = 'text';
+
+}
+//________________________________________________________________________________________
+function BCS(binArr,Bcs){
+    let decNo = 0;
+
+    binArr.pop();
+
+    binArr.forEach((item, index) =>  item === '1' ? decNo += Math.pow(2, index) : void 0);
+
+
+    if (Bcs[0] == 1) {
+        output3.value = "BCS: "+ Bcs + "   Decimal: -" + decNo.toString()  ;
+        output3.style.cursor = 'text';
+    }
+    else
+    {
+        output3.value = "BCS: "+ Bcs + "   Decimal: +" + decNo.toString()  ;
+        output3.style.cursor = 'text';
+    }
+
+}
+function BCS(binArr,Bcs){
+    let decNo = 0;
+
+    binArr.pop();
+
+    binArr.forEach((item, index) =>  item === '1' ? decNo += Math.pow(2, index) : void 0);
+
+
+    if (Bcs[0] == 1) {
+        output3.value = "BCS: "+ Bcs + "   Decimal: -" + decNo.toString()  ;
+        output3.style.cursor = 'text';
+    }
+    else
+    {
+        output3.value = "BCS: "+ Bcs + "   Decimal: +" + decNo.toString()  ;
+        output3.style.cursor = 'text';
+    }
+
+}
+//________________________________________________________________________________________
+
+//________________________________________________________________________________________
+
+function SacarDatos(numero) {
     const regEx = /^[0-1]+$/;
 
     const regEx2 = /^[0-1-2-3-4-5-6-7-8-9]+$/;
@@ -21,15 +74,12 @@ function Bin2Dec(numero) {
         
         Bss = Bss.replace(/\s|[,]/g,''); 
 
-        let decNo = 0;
-
-        binArr.forEach((item, index) => item === '1' ? decNo += Math.pow(2, index) : void 0);
-
-
-
-        output2.value = "BSS: "+ Bss + "   Decimal: " + decNo.toString()  ;
-        output2.style.cursor = 'text';
+        BSS(binArr,Bss);
+        BCS(binArr,Bss);
+        CA1((binArr,Bss));
     }
+
+
     else if (input_decimal.value.match(regEx2)){
         decArr = numero.split('').reverse();
         
@@ -39,8 +89,18 @@ function Bin2Dec(numero) {
 
 
 
-        output2.value = "BSS: "+ numero + "   Decimal: " + decNo.toString()  ;
+        output2.value = "BSS: 0"+ numero + "   Decimal: " + decNo.toString()  ;
         output2.style.cursor = 'text';
+
+        if (decNo > 0){
+            output3.value = "BCS: 0"+ numero + "   Decimal: " + decNo.toString()  ;
+            output3.style.cursor = 'text';
+            output4.value = "Ca1: 0"+ numero + "   Decimal: " + decNo.toString()  ;
+            output4.style.cursor = 'text';
+            output5.value = "Ca2: 0"+ numero + "   Decimal: " + decNo.toString()  ;
+            output5.style.cursor = 'text';
+
+        }
 
     }
     else {
@@ -48,40 +108,6 @@ function Bin2Dec(numero) {
     }
 }
 
-//________________________________________________________________________________________
-function BinarioSS() {
-    const regEx = /^[0-1]+$/;
-
-    if(input_binario.value.match(regEx)) {
-        let binArr = input_binario.value.split('').reverse();
-
-        let Bcs = input_binario.value.split('');
-        
-        let decNo = 0;
-
-        Bcs = Bcs.toString() ;
-        
-        Bcs = Bcs.replace(/\s|[,]/g,''); 
-
-        binArr.pop();
-
-        binArr.forEach((item, index) =>  item === '1' ? decNo += Math.pow(2, index) : void 0);
-
-
-        if (Bcs[0] == 1) {
-            output3.value = "BCS: "+ Bcs + "   Decimal: -" + decNo.toString()  ;
-            output3.style.cursor = 'text';
-        }
-        else
-        {
-            output3.value = "BCS: "+ Bcs + "   Decimal: +" + decNo.toString()  ;
-            output3.style.cursor = 'text';
-        }
-
-    }else {
-        error.style.display = 'block';
-    }
-}
 //________________________________________________________________________________________
 function convertToBinary (number, bin) {
     if (number > 0) {
@@ -115,6 +141,7 @@ btn.addEventListener('click', () => {
     let numero;
     numero=DecimalABinario(numero);
     console.log(numero);
-    Bin2Dec(numero);
-    BinarioSS();
+    SacarDatos(numero);
+    BinariosConSigno();
+    Ca1();
 })
