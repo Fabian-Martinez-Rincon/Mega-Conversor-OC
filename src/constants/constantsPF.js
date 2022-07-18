d0b47cf430a6b0360bc3fceac3e8ce61f8a2ffd5
Fabo xw <fabianmartinezrincon.123@gmail.com>
2022-07-18 18:36:54 -0300

    Reformando el todo el codigo constantes

---
+++ src/constants/constantsPF.js
@@ -0,0 +1,29 @@
+const INPUT_BINARIO = document.querySelector("#INPUT_BINARIO");
+const INPUT_DECIMAL = document.querySelector("#INPUT_DECIMAL");
+const INPUT_BIT = document.querySelector("#INPUT_BIT");
+
+const OUTPUT_BSS = document.querySelector("#OUTPUT_BSS");
+const OUTPUT_BCS = document.querySelector("#OUTPUT_BCS");
+const OUTPUT_CA1 = document.querySelector("#OUTPUT_CA1");
+const OUTPUT_CA2 = document.querySelector("#OUTPUT_CA2");
+const OUTPUT_EX2 = document.querySelector("#OUTPUT_EX2");
+const OUTPUT_BITS = document.querySelector("#OUTPUT_BITS");
+const OUTPUT_RANGO = document.querySelector("#OUTPUT_RANGO");
+const BTN = document.querySelector("#BTN");
+const ERROR = document.querySelector("#ERROR-MSG");
+
+const OUTPUT_MIN_MAX_NEG_BSS = document.querySelector("#OUTPUT_MIN_MAX_NEG_BSS");
+const OUTPUT_MIN_MAX_POS_BSS = document.querySelector("#OUTPUT_MIN_MAX_POS_BSS");
+
+const OUTPUT_MIN_MAX_NEG_BCS = document.querySelector("#OUTPUT_MIN_MAX_NEG_BCS");
+const OUTPUT_MIN_MAX_POS_BCS = document.querySelector("#OUTPUT_MIN_MAX_POS_BCS");
+
+const OUTPUT_MIN_MAX_NEG_CA1 = document.querySelector("#OUTPUT_MIN_MAX_NEG_CA1");
+const OUTPUT_MIN_MAX_POS_CA1 = document.querySelector("#OUTPUT_MIN_MAX_POS_CA1");
+
+const OUTPUT_MIN_MAX_NEG_CA2 = document.querySelector("#OUTPUT_MIN_MAX_NEG_CA2");
+const OUTPUT_MIN_MAX_POS_CA2 = document.querySelector("#OUTPUT_MIN_MAX_POS_CA2");
+
+const OUTPUT_MIN_MAX_NEG_EX2 = document.querySelector("#OUTPUT_MIN_MAX_NEG_EX2");
+const OUTPUT_MIN_MAX_POS_EX2 = document.querySelector("#OUTPUT_MIN_MAX_POS_EX2");
+
